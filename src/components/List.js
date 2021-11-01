e12b5c0c8872773af638c466677626e385a48717
Geonsik-Kim <web.gskim@gmail.com>
2021-11-02 00:07:53 +0900

    Refactor 불필요한 코드 삭제

--- src/components/List.js
+++ src/components/List.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { useRef, useEffect } from "react";
 import ListItem from "./ListItem";
 
 const ListWrap = styled.div`
@@ -9,18 +8,6 @@ const ListWrap = styled.div`
 `;
 
 const List = ({ movieItem }) => {
-  // 렌더링이 되어도 값을 보존하기 위해 useRef를 사용
-  const mounted = useRef(false);
-
-  // setMovieItemm 즉 useState는 비동기 방식으로 작동하기에 useEffect를 사용하여 바로 적용하여 확인 할 수있도록 함
-  useEffect(() => {
-    if (!mounted.current) {
-      mounted.current = true;
-    } else {
-      console.log(movieItem);
-    }
-  }, [movieItem]);
-
   return (
     <ListWrap>
       {movieItem.map((item, index) => (
