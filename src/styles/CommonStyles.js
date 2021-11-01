71e5c1560da767621d0ec929383ffdc3abd347d7
Geonsik-Kim <web.gskim@gmail.com>
2021-11-01 15:16:50 +0900

    Add 공통 스타일

---
+++ src/styles/CommonStyles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Blind = styled.span`
+  display: inline-block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  font-size: 0;
+  line-height: 0;
+  border: 0;
+  overflow: hidden;
+`;
